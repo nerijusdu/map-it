ebbe2d7c0b4d4b3dfb4d6d5767f2febfb93e08e2
Nerijus Dulke <dnerijus72@gmail.com>
2019-12-23 22:07:11 +0200

    move paged-list to separate component

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,93 +1,51 @@
 <template>
   <div class="logs-container">
-    <md-table>
-      <md-table-toolbar class="toolbar">
-        <h1>Logs</h1>
-        <div class="page-controls">
-          <img src="@/assets/arrow-left.svg" v-show="page !== 1" @click="changePage(page - 1)"/>
-          <div>Page: {{page}} of {{totalPages}}</div>
-          <img src="@/assets/arrow-right.svg" v-show="page !== totalPages" @click="changePage(page + 1)"/>
-        </div>
+    <paged-list
+      title="Logs"
+      uniqueColumn="log_id"
+      :fetchDataFunc="api.getLogs"
+      :columns="[
+        { name: 'Log ID', field: 'log_id' },
+        { name: 'Message', field: 'message' },
+        { name: 'Level', field: 'level' },
+        { name: 'Timestamp', field: 'timestamp' }
+      ]"
+      :filterValues="{ onlyErrors: false }"
+    >
+      <template v-slot:filters="props">
         <div class="checkbox">
-          <md-checkbox v-model="onlyErrors" @change="getLogs(true)"/>
+          <md-checkbox
+            v-model="props.filters.onlyErrors"
+            @change="(checked) => {
+              props.getData({
+                resetPaging: true,
+                filters: {
+                  level: checked ? 'error' : undefined
+                }
+              });
+            }"/>
           <label>Only errors</label>
         </div>
-      </md-table-toolbar>
-      <md-table-row class="header">
-        <md-table-head>Message</md-table-head>
-        <md-table-head>Level</md-table-head>
-        <md-table-head>Timestamp</md-table-head>
-      </md-table-row>
-      <md-table-row v-for="log in logs" v-bind:key="log.log_id">
-        <md-table-cell class="clickable">{{ log.message }}</md-table-cell>
-        <md-table-cell class="clickable">{{ log.level }}</md-table-cell>
-        <md-table-cell class="clickable">{{ log.timestamp }}</md-table-cell>
-      </md-table-row>
-    </md-table>
+      </template>
+    </paged-list>
   </div>
 </template>
 
 <script>
-import { datePreviewFormat } from '../constants';
+import PagedList from './PagedList';
 import api from '../services/api';
 
 export default {
   data: () => ({
-    datePreviewFormat,
-    logs: [],
-    onlyErrors: false,
-    page: 1,
-    totalPages: 1
+    api
   }),
-  methods: {
-    changePage(page) {
-      if (page > 0 && page <= this.totalPages) {
-        this.page = page;
-        this.getLogs();
-      }
-    },
-    async getLogs(resetPaging) {
-      if (resetPaging) {
-        this.page = 1;
-      }
-
-      const res = await api.getLogs({
-        level: this.onlyErrors ? 'error' : undefined,
-        page: this.page,
-        pageSize: 5
-      }, {
-        ignoreLoading: true
-      });
-      if (res.ok) {
-        this.logs = res.data.items;
-        this.totalPages = res.data.pageCount;
-      }
-    }
-  },
-  created() {
-    this.getLogs();
+  components: {
+    PagedList
   }
 };
 </script>
 
 <style scoped>
-.toolbar {
-  display: flex;
-  justify-content: space-between;
-}
-
-.page-controls {
-  display: flex;
-}
-
-.page-controls > div {
-  margin: 5px;
-}
-
-.page-controls > img {
-  cursor: pointer;
-}
-
 .checkbox {
   display: flex;
   align-items: center;
---
+++ src/components/PagedList.vue
@@ -0,0 +1,109 @@
+<template>
+  <md-table>
+    <md-table-toolbar class="toolbar">
+      <h1>{{title}}</h1>
+      <div class="page-controls">
+        <img src="@/assets/arrow-left.svg" v-show="page !== 1" @click="changePage(page - 1)"/>
+        <div>Page: {{page}} of {{totalPages}}</div>
+        <img src="@/assets/arrow-right.svg" v-show="page !== totalPages" @click="changePage(page + 1)"/>
+      </div>
+      <div class="additional-filters">
+        <slot
+          name="filters"
+          :getData="getData"
+          :filters="filterValues"
+        ></slot>
+      </div>
+    </md-table-toolbar>
+    <md-table-row class="header">
+      <md-table-head v-for="c in columns" :key="c.field+c.name">
+        {{c.name}}
+      </md-table-head>
+    </md-table-row>
+    <md-table-row v-for="dataItem in data" :key="dataItem[uniqueColumn]">
+      <md-table-cell class="clickable"  v-for="c in columns" :key="c.field+c.name">
+        {{ dataItem[c.field] }}
+      </md-table-cell>
+    </md-table-row>
+  </md-table>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    },
+    uniqueColumn: {
+      type: String,
+      required: true
+    },
+    fetchDataFunc: {
+      type: Function,
+      required: true
+    },
+    filterValues: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data: () => ({
+    data: [],
+    page: 1,
+    totalPages: 1
+  }),
+  methods: {
+    changePage(page) {
+      if (page > 0 && page <= this.totalPages) {
+        this.page = page;
+        this.getData();
+      }
+    },
+    async getData({ resetPaging, filters } = { filters: {} }) {
+      if (resetPaging) {
+        this.page = 1;
+      }
+
+      const res = await this.fetchDataFunc({
+        page: this.page,
+        pageSize: 5,
+        ...filters
+      }, {
+        ignoreLoading: true
+      });
+
+      if (res.ok) {
+        this.data = res.data.items;
+        this.totalPages = res.data.pageCount;
+      }
+    }
+  },
+  created() {
+    this.getData();
+  }
+};
+</script>
+
+<style scoped>
+.toolbar {
+  display: flex;
+  justify-content: space-between;
+}
+
+.page-controls, .additional-filters {
+  display: flex;
+}
+
+.page-controls > div {
+  margin: 5px;
+}
+
+.page-controls > img {
+  cursor: pointer;
+}
+</style>
